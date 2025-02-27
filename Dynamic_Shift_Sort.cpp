26321739829c21fca1b8046a5512d3bf799a5da1
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-27 19:44:20 +0000

    uploaded the code

---
+++ Dynamic_Shift_Sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+// Dynamic Shift Sort Algorithm
+void dynamicShiftSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] < arr[j]) { // Swap to shift larger elements forward
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {5, 47, 65, 12, 2};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Initial array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    dynamicShiftSort(arr, size);
+
+    cout << "Sorted array (Descending): ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
